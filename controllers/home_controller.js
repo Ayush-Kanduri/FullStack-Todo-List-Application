39b0cc3bf6983035fab5d74361425603662adabf
Ayush <ayush.kanduri@gmail.com>
2022-03-08 15:38:40 +0530

    controllers added

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = (req, res) => {
+	return res.end("<h1>Home Page</h1>");
+};
---
+++ controllers/users_controller.js
@@ -0,0 +1,3 @@
+module.exports.users = (req, res) => {
+	return res.end("<h1>User Profile Page</h1>");
+};
