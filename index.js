8704a8eaeec8d2a96def38041bde94a0869c67f8
Ayush <ayush.kanduri@gmail.com>
2022-03-08 17:10:04 +0530

    layout Rendered

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const port = 8000;
 const app = express();
 const path = require("path");
 const route = require("./routes/index.js");
-// const expressLayouts = require("express-ejs-layouts");
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(expressLayouts);
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/", route);
