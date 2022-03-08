958d9f16d2609f3d9ed5615238408c679535c5b4
Ayush <ayush.kanduri@gmail.com>
2022-03-08 17:59:13 +0530

    Partials, Layouts & Static Files rendered

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const path = require("path");
 const route = require("./routes/index.js");
 const expressLayouts = require("express-ejs-layouts");
 
+app.use(express.static("./assets"));
 app.use(expressLayouts);
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/", route);
