e95a251d1c056f2d97c2eb5d3410781809069558
Ayush <ayush.kanduri@gmail.com>
2022-03-08 15:50:29 +0530

    index route modified & mapped to other sub routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const homeRouterFile = require("./home");
+const usersRouterFile = require("./users");
+
+router.use("/", homeRouterFile);
+router.use("/users", usersRouterFile);
+
 console.log("Router Loaded");
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const usersController = require("../controllers/usersController");
+const usersController = require("../controllers/users_controller");
 
 router.get("/profile", usersController.profile);
 
